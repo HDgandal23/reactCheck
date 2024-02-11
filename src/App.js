c39b739b9b2acf3e183e76ec225ce2de6234740e
Habib <habibdiallohd08@gmail.com>
2024-02-11 18:40:26 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,86 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     
+     <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
+            
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+
+    <div className='App'>
+      <div>
+        <Card style={{ width: '18rem' }}>
+            <Card.Body>
+            <Card.Title>Card Title</Card.Title>
+            <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+            </Card.Text>
+        <Button variant="primary">Go somewhere</Button>
+          </Card.Body>
+        </Card>
+      </div>
+    </div>
+    <div>
+        <Card style={{ width: '18rem' }}>
+            <Card.Body>
+            <Card.Title>Card Title</Card.Title>
+            <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+            </Card.Text>
+        <Button variant="primary">Go somewhere</Button>
+          </Card.Body>
+        </Card>
+     
+    </div>
+    <div>
+        <Card style={{ width: '18rem' }}>
+            <Card.Body>
+            <Card.Title>Card Title</Card.Title>
+            <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+            </Card.Text>
+        <Button variant="primary">Go somewhere</Button>
+          </Card.Body>
+        </Card>
+     
+    </div>
+    <div>
+        <Card style={{ width: '18rem' }}>
+            <Card.Body>
+            <Card.Title>Card Title</Card.Title>
+            <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+            </Card.Text>
+        <Button variant="primary">Go somewhere</Button>
+          </Card.Body>
+        </Card>
+     
+    </div>
+  
+
     </div>
   );
 }
